f759d99fa9d3770326d517de9682cf55dab48d73
PetrovitchSharp <korchagin_official@yahoo.com>
2021-04-03 21:48:13 +0400

    feat: added existence check for PUT and POST requests

--- server/db/routers/customerRouter.ts
+++ server/db/routers/customerRouter.ts
@@ -1,6 +1,7 @@
 const express = require('express');
 const customerRouter = express.Router();
 const Customer = require("../schemas/CustomerSchema");
+const utils = require("../dbUtils");
 
 customerRouter.get("/customers",(req,res)=>{
     Customer.find({})
@@ -10,20 +11,30 @@ customerRouter.get("/customers",(req,res)=>{
 });
 
 customerRouter.post("/customers",(req,res)=>{
-    Customer.create(req.body)
-        .then(customer =>{
-        res.send(customer);
-    });
+    if (!utils.checkExist(req.body.login)) {
+        Customer.create(req.body)
+            .then(customer => {
+                res.send(customer);
+            });
+    }
+    else {
+        res.send("User with this login is already exists");
+    }
 });
 
 customerRouter.put("/customers/:id",(req,res)=>{
-    Customer.findByIdAndUpdate({_id: req.params.id},req.body)
-        .then(()=>{
-            Customer.findOne({_id: req.params.id})
-                .then(customer =>{
-                    res.send(customer);
-                });
-        });
+    if (!utils.checkExist(req.body.login)) {
+        Customer.findByIdAndUpdate({_id: req.params.id}, req.body)
+            .then(() => {
+                Customer.findOne({_id: req.params.id})
+                    .then(customer => {
+                        res.send(customer);
+                    });
+            });
+    }
+    else {
+        res.send("User with this login is already exists");
+    }
 });
 
 customerRouter.delete("/customers/:id",(req,res)=>{
