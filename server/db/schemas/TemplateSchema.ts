48efb5ce723c1739960893a1796b89dbade859c8
PetrovitchSharp <korchagin_official@yahoo.com>
2021-04-13 13:59:01 +0400

    fix: updated entities schemas

--- server/db/schemas/TemplateSchema.ts
+++ server/db/schemas/TemplateSchema.ts
@@ -1,9 +1,9 @@
 import {Schema,model} from "mongoose";
 
 const TemplateSchema = new Schema({
-    template_name: String,
-    template_price: Number,
-    template_descr: String,
+    template_name: {type: String, default: ""},
+    template_price: {type: Number, default: 0},
+    template_descr: {type: String, default: ""},
 });
 
 const Template = model("template",TemplateSchema);
