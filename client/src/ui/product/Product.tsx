915fcfa53c068236c88555dfd2dcb19caec4504e
DevMrSerJ <58230079+DevMrSerJ@users.noreply.github.com>
2021-04-24 13:55:40 +0400

    edit Product.tsx in client

--- client/src/ui/product/Product.tsx
+++ client/src/ui/product/Product.tsx
@@ -1,17 +1,15 @@
 import React from "react";
-import { useDispatch } from "react-redux";
 import { ProductProps } from "./ProductProps";
-import { addItemToCart } from "../shopping-cart/ShoppingCartActions";
 
 export const Product: React.FC<ProductProps> = (props) => {
-  const dispatch = useDispatch();
-  const { img, name, price, id } = props;
+  const { imgSrc, name, price, desc } = props;
   return (
     <div className="productBorder">
-      <img className="productImage" alt="SomeImage" src={img} />
+      <img className="productImage" alt="SomeImage" src={imgSrc} />
       <section>
         <h3>{name}</h3>
         <h3>${price}</h3>
+        <h3>{desc}</h3>
       </section>
     </div>
   );
