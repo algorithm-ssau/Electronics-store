f3a423fcdce660be157903df6c0e1d904d13f0d4
DevMrSerJ <58230079+DevMrSerJ@users.noreply.github.com>
2021-04-24 11:04:00 +0400

    edit port in server.ts

--- server/server.ts
+++ server/server.ts
@@ -37,7 +37,7 @@ app.use("/api",orderRouter)
 app.use("/api",templateRouter)
 app.use("/api",productRouter)
 
-const PORT= process.env.PORT || 6000
+const PORT= process.env.PORT || 5000
 app.listen(PORT,()=>{
     console.log('Server is running on port',PORT)
 })
