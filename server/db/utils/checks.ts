74ba04d1c9e9f106f2eaa73f7c46a3339126b163
PetrovitchSharp <korchagin_official@yahoo.com>
2021-04-27 21:34:16 +0400

    fix: added function to check email existence, removed password check

--- server/db/utils/checks.ts
+++ server/db/utils/checks.ts
@@ -1,26 +1,23 @@
 import {Customer} from "../schemas/CustomerSchema";
 
 /**
- * Utility function to check existence of current user in database
+ * Utility function to check existence of current user's login in database
  * @param login Login of current user
  * @returns true if user with current login not exists, false otherwise
  */
-async function checkExist(login: String):Promise<Boolean>{
-    let customer = Customer.findOne({login: login});
-    return customer.countDocuments().then((res)=>(res==0));
+async function checkLoginExist(login: String):Promise<Boolean>{
+    let customers = Customer.findOne({login: login});
+    return customers.countDocuments().then((res)=>(res==0))
 }
-
 
 /**
- * Utility function to authenticate current user in database
- * @param login Login of current user
- * @param password Password of current user
- * @returns true if user's password is correct, false otherwise
+ * Utility function to check existence of current user's email in database
+ * @param email Email of current user
+ * @returns true if user with current login not exists, false otherwise
  */
-async function checkPassword(login:String, password:String ):Promise<Boolean>{
-    let customer = Customer.findOne({login: login});
-    return (customer.get('password').then((res)=>{(res==password)}))
+async function checkEmailExist(email: String):Promise<Boolean>{
+    let customers = Customer.findOne({email: email});
+    return customers.countDocuments().then((res)=>(res==0))
 }
 
-
-export {checkExist,checkPassword}
+export {checkLoginExist,checkEmailExist}
