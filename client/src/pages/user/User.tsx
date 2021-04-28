00a5e8e9a67cce7835088237616316c8e68e3cd8
DevMrSerJ <58230079+DevMrSerJ@users.noreply.github.com>
2021-04-28 23:35:10 +0400

    added binding of styles with html blocks

--- client/src/pages/user/User.tsx
+++ client/src/pages/user/User.tsx
@@ -1,3 +1,46 @@
 import React from "react";
 
-export const User = () => <h1>Страница юзера</h1>;
+export const User = () => (
+  <div>
+    <div className="dataUser">
+      <div className="singleLineBlocks">
+        <img
+          className="userImage"
+          src="https://pereezd-neva.ru/assets/template/img/spectehnika/d5fa0e2330973dc6b7571d881a345d6b.png"
+          alt="аватар юзера"
+        />
+      </div>
+      <div className="singleLineBlocks">
+        <div className="userDescription">
+          <h5>Логин: ivanov.Ivan</h5>
+          <h5>Имя: Иван</h5>
+          <h5>Фамилия: Иванов</h5>
+          <h5>Отчетство: Иванович</h5>
+          <h5>Баланс: $ 100</h5>
+        </div>
+      </div>
+    </div>
+    <div className="purchaseHistory">
+      <h2>История покупок</h2>
+      <h3>У вас было 2 покупки</h3>
+    </div>
+    <div className="dataPurchase">
+      <table className="tablePurchaseHistory">
+        <tbody>
+          <tr className="nameColumn">
+            <td>Название товара</td>
+            <td>Сумма сделки</td>
+          </tr>
+          <tr>
+            <td>Смартфон Lenovo</td>
+            <td>$ 200</td>
+          </tr>
+          <tr>
+            <td>Ноутбук Acer</td>
+            <td>$ 900</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+);
