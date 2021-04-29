f5f3c3d9dbd3c0686e346dcab9e0959c0595ec3c
DevMrSerJ <58230079+DevMrSerJ@users.noreply.github.com>
2021-04-29 23:02:46 +0400

    added binding styles to blocks

--- client/src/pages/registration/Registration.tsx
+++ client/src/pages/registration/Registration.tsx
@@ -1,3 +1,27 @@
 import React from "react";
+import { addItemToCart } from "../../ui/shopping-cart/ShoppingCartActions";
 
-export const Registration = () => <h1>Страница регистрации</h1>;
+export const Registration = () => (
+  <div>
+    <div className="loginInForm">
+      <span className="textLogIn">вход</span>
+      <input type="text" placeholder="E-mail" className="inputDataRegistration" />
+      <input type="password" placeholder="Password" className="inputDataRegistration" />
+      <button type="button" className="loginIn">
+        <span className="descriptionLogIn">ВОЙТИ</span>
+      </button>
+      <span className="buttonRegistration">зарегистрироваться</span>
+    </div>
+    <div className="loginInForm">
+      <span className="textLogIn">регистрация</span>
+      <input type="text" placeholder="Фамилия" className="inputDataRegistration" />
+      <input type="text" placeholder="Имя" className="inputDataRegistration" />
+      <input type="text" placeholder="Отчество" className="inputDataRegistration" />
+      <input type="text" placeholder="E-mail" className="inputDataRegistration" />
+      <input type="password" placeholder="Password" className="inputDataRegistration" />
+      <button type="button" className="registrationButton">
+        <span className="descriptionLogIn">зарегистрироваться</span>
+      </button>
+    </div>
+  </div>
+);
