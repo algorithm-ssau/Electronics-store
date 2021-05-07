92a942de3f85f185a7d4950a3bd6c3a894dca95f
PetrovitchSharp <korchagin_official@yahoo.com>
2021-05-07 21:58:55 +0400

    feat: added response type to every message from python api

--- server/main.py
+++ server/main.py
@@ -8,28 +8,42 @@ environment = dotenv_values('.env')
 
 @app.get('/api/appinfo/server/version')
 async def get_server_version():
-    return {"server_version":environment['SERVER_VERSION']}
+    return [{
+        "responseType":"Data",
+        "server_version":environment['SERVER_VERSION']
+    }]
 
 @app.get('/api/appinfo/client/version')
 async def get_client_version():
-    return {"client_version":environment['CLIENT_VERSION']}
+    return [{
+        "responseType":"Data",
+        "client_version":environment['CLIENT_VERSION']
+    }]
 
 @app.get('/api/appinfo/server/last_update')
 async def get_date_of_server_last_update():
-    return {"server_last_update":environment['SERVER_LAST_UPDATE']}
+    return [{
+        "responseType":"Data",
+        "server_last_update":environment['SERVER_LAST_UPDATE']
+    }]
 
 @app.get('/api/appinfo/client/last_update')
 async def get_date_of_client_last_update():
-    return {"client_last_update":environment['CLIENT_LAST_UPDATE']}
+    return [{
+        "responseType":"Data",
+        "client_last_update":environment['CLIENT_LAST_UPDATE']
+    }]
 
 @app.get('/api/appinfo/developers')
 async def get_developers():
-    return {"Team Lead":"Абрамов Кирилл Дмитриевич",
-            "Database Engineers":["Корчагин Петр Петрович","Абрамов Кирилл Дмитриевич"],
-            "Frontend Developers":["Кротков Владимир Денисович",
-                                   "Иванов Сергей Юрьевич",
-                                   "Бужлаков Никита Сергеевич"],
-            "Backend Developers":["Корчагин Петр Петрович"]}
+    return [{
+        "responseType":"Data",
+        "Team Lead":"Абрамов Кирилл Дмитриевич",
+        "Database Engineers":["Корчагин Петр Петрович","Абрамов Кирилл Дмитриевич"],
+        "Frontend Developers":["Кротков Владимир Денисович",
+                               "Иванов Сергей Юрьевич",
+                               "Бужлаков Никита Сергеевич"],
+        "Backend Developers":["Корчагин Петр Петрович"]}]
 
 
 
