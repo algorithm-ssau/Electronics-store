d2c0f9337c227c003453c1ca0e6cebd5d4530997
PetrovitchSharp <korchagin_official@yahoo.com>
2021-05-10 13:11:07 +0400

    feat: modified post request for Orders

--- server/db/routers/orderRouter.ts
+++ server/db/routers/orderRouter.ts
@@ -2,7 +2,9 @@ import {Router} from 'express';
 import {Order} from "../schemas/OrderSchema";
 import {getTotal, parseOrders} from "../utils/orderParser";
 import * as aqp from 'api-query-params';
-import {getOrderId} from "../utils/getIDs";
+import {getCustomerId, getOrderId} from "../utils/getIDs";
+import {checkAccount} from "../utils/checks";
+import {Customer} from "../schemas/CustomerSchema";
 
 const orderRouter = Router();
 
@@ -27,11 +29,40 @@ orderRouter.get("/orders/get",(req,res)=>{
 });
 
 orderRouter.post("/orders/post",async(req,res)=>{
-    req.body.total = await getTotal(req.body.products);
-    Order.create(req.body)
-        .then(order =>{
-            res.send(parseOrders([order]));
-        });
+    const customerId = await getCustomerId({email: req.body.email, password: req.body.password});
+    if (customerId!=null) {
+        const total: number = await getTotal(req.body.products);
+        const accountFlag = await checkAccount(req.body.email,req.body.password,total);
+        if (accountFlag) {
+            const order = {
+                status: "Принят в обработку",
+                total: total,
+                products: req.body.products
+            }
+            Order.create(order)
+                .then(async order => {
+                    const orderId = order._id;
+                    let customer = await Customer.findOne({email: req.body.email, password: req.body.password});
+                    customer.set('account', customer.get('account') - total);
+                    let customerOrders = customer.get('order_ids');
+                    customerOrders.push(orderId);
+                    customer.set('order_ids', customerOrders);
+                    Customer.findByIdAndUpdate({_id: customerId}, customer)
+                        .then(() => {
+                            res.send(parseOrders([order]));
+                        });
+                });
+        } else res.send([{
+            responseType: "Message",
+            error: true,
+            message: "Customer hasn't got enough money to pay"
+        }])
+    }
+    else res.send([{
+        responseType: "Message",
+        error: true,
+        message: "Customer not found"
+    }])
 });
 
 orderRouter.put("/orders/update",async(req,res)=>{
