ceb8dd697cceb40ccea7d8feed7959adc19834fd
PetrovitchSharp <korchagin_official@yahoo.com>
2021-05-10 14:42:48 +0400

    fix: backend update

--- server/db/responseInterfaces/customerInterfaces.ts
+++ server/db/responseInterfaces/customerInterfaces.ts
@@ -14,6 +14,7 @@ export interface UserDataProps {
     realName: String;
     account: Number;
     orders: String[];
+    isAdmin: Boolean;
 }
 
 export interface CustomerResponse extends Document{
