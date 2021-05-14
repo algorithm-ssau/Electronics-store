49fc46759944aa54b1a37674d1edd02f7f60cce3
PetrovitchSharp <korchagin_official@yahoo.com>
2021-05-14 13:22:52 +0400

    feat: set default avatar for customers

--- server/db/schemas/CustomerSchema.ts
+++ server/db/schemas/CustomerSchema.ts
@@ -7,7 +7,7 @@ const CustomerSchema = new Schema({
     login: {type:String, default: ""},
     password: {type: String, default: ""},
     account: {type: Number, default: 10000},
-    avatar_src: {type: String, default: ""},
+    avatar_src: {type: String, default: "https://media.wired.com/photos/5cdefb92b86e041493d389df/1:1/w_988,h_988,c_limit/Culture-Grumpy-Cat-487386121.jpg"},
     order_ids: {type: [String], default: []},
     verified: {type: Boolean, default: false},
 });
