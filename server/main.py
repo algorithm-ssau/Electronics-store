368d7b9035f0b74aabfbc7ba5764b9c15768078f
Petr <korchagin_official@yahoo.com>
2021-05-15 13:56:45 +0400

    feat: added cors policy

--- server/main.py
+++ server/main.py
@@ -1,12 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import dotenv_values
 import json
 import setproctitle as spt
+
 
 spt.setproctitle("pyapi")
 print("Current process title: ", spt.getproctitle())
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 environment = dotenv_values('.env')
 
