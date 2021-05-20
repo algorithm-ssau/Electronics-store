ecb4a689a27172a6eb254ac231dc657260e9243a
PetrovitchSharp <korchagin_official@yahoo.com>
2021-05-20 17:24:22 +0400

    fix: added new error "order is empty"

--- server/db/routers/orderRouter.ts
+++ server/db/routers/orderRouter.ts
@@ -33,44 +33,51 @@ orderRouter.get("/orders/get",(req,res)=>{
 
 orderRouter.post("/orders/post", cors())
 orderRouter.post("/orders/post",cors(), async(req,res)=>{
-    const customerId = await getCustomerId({email: req.body.email, password: req.body.password});
-    if (customerId!=null) {
-        const total: number = await getTotal(req.body.products);
-        const accountFlag = await checkAccount(req.body.email,req.body.password,total);
-        if (accountFlag) {
-            const order = {
-                status: "Принят в обработку",
-                total: total,
-                products: req.body.products
-            }
-            Order.create(order)
-                .then(async order => {
-                    const orderId = order._id;
-                    let customer = await Customer.findOne({email: req.body.email, password: req.body.password});
-                    customer.set('account', customer.get('account') - total);
-                    let customerOrders = customer.get('order_ids');
-                    customerOrders.push(orderId);
-                    customer.set('order_ids', customerOrders);
-                    Customer.findByIdAndUpdate({_id: customerId}, customer)
-                        .then(() => {
-                            res.send([{
-                                responseType: "Message",
-                                error: false,
-                                message: "Order was successfully created"
-                            }])
-                        });
-                });
+    if (req.body.products.length == 0){
+        res.send([{
+            responseType: "Message",
+            error: true,
+            message: "Order is empty"}]);
+    }
+    else {
+        const customerId = await getCustomerId({email: req.body.email, password: req.body.password});
+        if (customerId != null) {
+            const total: number = await getTotal(req.body.products);
+            const accountFlag = await checkAccount(req.body.email, req.body.password, total);
+            if (accountFlag) {
+                const order = {
+                    status: "Принят в обработку",
+                    total: total,
+                    products: req.body.products
+                }
+                Order.create(order)
+                    .then(async order => {
+                        const orderId = order._id;
+                        let customer = await Customer.findOne({email: req.body.email, password: req.body.password});
+                        customer.set('account', customer.get('account') - total);
+                        let customerOrders = customer.get('order_ids');
+                        customerOrders.push(orderId);
+                        customer.set('order_ids', customerOrders);
+                        Customer.findByIdAndUpdate({_id: customerId}, customer)
+                            .then(() => {
+                                res.send([{
+                                    responseType: "Message",
+                                    error: false,
+                                    message: "Order was successfully created"
+                                }])
+                            });
+                    });
+            } else res.send([{
+                responseType: "Message",
+                error: true,
+                message: "Customer hasn't got enough money to pay"
+            }])
         } else res.send([{
             responseType: "Message",
             error: true,
-            message: "Customer hasn't got enough money to pay"
+            message: "Customer not found"
         }])
     }
-    else res.send([{
-        responseType: "Message",
-        error: true,
-        message: "Customer not found"
-    }])
 });
 
 orderRouter.put("/orders/update", cors())
